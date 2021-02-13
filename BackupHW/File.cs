5a5397e0329b14b9c8fc23568c5b5b4401b588e6
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-13 17:47:02 +0600

    BackupHW

---
+++ BackupHW/File.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackupHW
+{
+    public class File
+    {
+        private double _size;
+
+        public File()
+        {
+            Size = 780;
+        }
+
+        public File(double size)
+        {
+            Size = size;
+        }
+
+        public double Size
+        {
+            get { return _size; }
+            set { _size = value; }
+        }
+    }
+}
